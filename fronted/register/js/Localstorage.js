9c0ecc0089bace4d65105a6274cb228fe1aa32b8
oyj135 <107733358+oyj135@users.noreply.github.com>
2024-08-30 00:24:57 +0800

    Delete fronted directory

--- fronted/register/js/Localstorage.js
+++
@@ -1,9 +0,0 @@
-
-	// document.getElementById("tj1").onclick=function(){
-	// var userName=document.getElementById("userName").value;
-	// var userPwd=document.getElementById("userPwd").value;
-	// window.localStorage.setItem('userName',userName);
-	// window.localStorage.setItem('userPwd',userPwd);
-	// }
-
-
